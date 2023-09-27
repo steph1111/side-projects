b95ce1ad5422128b715c08c6909584d19f2cbec7
Liam Gilligan <liamdgilligan@gmail.com>
2023-09-27 16:52:58 -0700

    made GOOD python code

--- simpsons_rule/simpson.py
+++ simpsons_rule/simpson.py
@@ -1,2 +1,11 @@
-def simpson(x_0:float, x_n:float, func:function, n:int=100)->float:
-  pass 
+#!/usr/bin/env python3
+def simpson(f) -> float:
+    return (1/3) * (f[0] + 4 * f[1] + 2 * f[2] + 4 * f[3] + f[4])
+
+
+def main():
+    data = [1, 2, 3, 4, 5]
+    print(simpson(data))
+
+if __name__ == "__main__":
+    main()
