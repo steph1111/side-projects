1fee4ac32094106ae7d2b855bce43f6fd5be97ff
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-10-10 12:28:58 -0700

    somehow it is better

--- bad_for/bad_for.cpp
+++ bad_for/bad_for.cpp
@@ -4,8 +4,8 @@ int main () {
   const int LEN_I = 4, LEN_J = 3;
   int arr[LEN_I][LEN_J] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
   
-  for (int i = 0, j = 0; i < LEN_I; j++, i += int(j % LEN_J == 0)) {
-    std::cout << arr[i][j % LEN_J] << std::endl;
+  for (int i = 0, j = 0; i < LEN_I; j = (j + 1) % LEN_J, i += int(j == 0)) {
+    std::cout << arr[i][j] << std::endl;
   }
 
   return 0;
